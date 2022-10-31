8ad03c5351f64f6adbb9f34bd924684c366ed724
André <alfr2@cin.ufpe.br>
2022-10-30 23:53:09 -0300

    design estático da home page concluido

--- web-app/src/components/Banner.tsx
+++ web-app/src/components/Banner.tsx
@@ -7,8 +7,11 @@ import {FaTwitter,
         FaBook,
         FaUserAlt,
         FaRegCommentDots,
-        FaSearch
+        FaSearch,
+        FaLock
         } from 'react-icons/fa';
+import {BsThreeDots
+       } from 'react-icons/bs';
 
 import { useMediaQuery } from 'react-responsive';
 import { useState } from 'react';
@@ -19,12 +22,22 @@ function Banner() {
 
     const dispatch = useDispatch();
 
-    function handleLogOut(){
+    function handleLogOut () {
         console.log("trying to log out");
         dispatch(removeUser());
 
         localStorage.clear();
     } 
+
+    function showHiddenOptionsFromAvatarContainer () {
+        const el = document.getElementsByClassName('User-avatar-container-hidden-options');
+        el[0].classList.remove('Hidden');
+    }
+
+    function hideOptionsFromAvatarContainer () {
+        const el = document.getElementsByClassName('User-avatar-container-hidden-options');
+        el[0].classList.add('Hidden');
+    }
 
     const isSmallScreen = useMediaQuery({ query: '(max-width: 720px)' });
     const isMediumScreen = useMediaQuery({ query: '(max-width: 1100px)' });
@@ -46,14 +59,14 @@ function Banner() {
                         <FaRegBell className='Banner-menu-icon Icon-large' id="icon-notificacoes"/>
                     </div>
                     <div className='Icon-container btn-themed-white-black'>
-                    <FaRegEnvelope className='Banner-menu-icon Icon-large' id="icon-mensagens"/>
+                        <FaRegEnvelope className='Banner-menu-icon Icon-large' id="icon-mensagens"/>
                     </div>
                     
                 </div>
             </header>
         );
 
-    if(isMediumScreen)
+    if(isLargeScreen)
         return (
             <header className='Banner Banner-flex-column Banner-small'>
             
@@ -65,9 +78,15 @@ function Banner() {
                     <div className='Icon-container btn-themed-white-black'>
                         <FaHouseUser className='Banner-menu-icon Icon-large' id="icon-home"/>
                     </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaSearch className='Banner-menu-icon Icon-large' id="icon-search"/>
-                    </div>
+                    {isMediumScreen?
+                        <div className='Icon-container btn-themed-white-black'>
+                            <FaSearch className='Banner-menu-icon Icon-large' id="icon-search"/>
+                        </div>
+                        :
+                        <div className='Icon-container btn-themed-white-black'>
+                            <FaSlackHash className='Banner-menu-icon Icon-large' id="icon-explorar"/>
+                        </div>
+                    }
                     <div className='Icon-container btn-themed-white-black'>
                         <FaRegBell className='Banner-menu-icon Icon-large' id="icon-notificacoes"/>
                     </div>
@@ -88,52 +107,23 @@ function Banner() {
                     </div>
 
                 </div>
-                <img alt='user profile pic'></img>
-            </header>
-        );
-    
-    if(isLargeScreen)
 
-        return (
-            <header className='Banner Banner-flex-column Banner-small'>
-                <div className='Banner-menu Banner-menu-flex-column'>
-
-                    <div className='Icon-container btn-themed-white-blue'>
-                        <FaTwitter className='Banner-menu-icon Icon-large' id="icon-twitter" fill='rgb(29, 155, 240)'/>
+                <div className='User-avatar-container btn-themed-white-black' onClick={showHiddenOptionsFromAvatarContainer}>
+                    <img className='User-avatar-container-icon Icon-xl' 
+                         alt='user profile pic' 
+                         src='http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png'/>
+                    <div className='User-avatar-container-hidden-options Hidden'>
+                        <span>Adicionar uma conta existente</span>
+                        <span>Sair de {'@andrezzz'}</span>
                     </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaHouseUser className='Banner-menu-icon Icon-large' id="icon-home"/>
-                    </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaSlackHash className='Banner-menu-icon Icon-large' id="icon-explorar"/>
-                    </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaRegBell className='Banner-menu-icon Icon-large' id="icon-notificacoes"/>
-                    </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaRegEnvelope className='Banner-menu-icon Icon-large' id="icon-mensagens"/>
-                    </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaRegBookmark className='Banner-menu-icon Icon-large' id="icon-itens-salvos"/>
-                    </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaBook className='Banner-menu-icon Icon-large' id="icon-listas"/>
-                    </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaUserAlt className='Banner-menu-icon Icon-large' id="icon-perfil"/>
-                    </div>
-                    <div className='Icon-container btn-themed-white-black'>
-                        <FaRegCommentDots className='Banner-menu-icon Icon-large' id="icon-listas"/>
-                    </div>
-                    
                 </div>
-                <img alt='user profile pic'></img>
+
             </header>
         );
 
     else
         return (
-            <header className='Banner Banner-flex-column Banner-large'>
+            <header className='Banner Banner-flex-column Banner-large' onClick={hideOptionsFromAvatarContainer}>
 
                 <div className='Banner-menu Banner-menu-flex-column'>
 
@@ -185,7 +175,21 @@ function Banner() {
 
                 </div>
 
-                <img alt='user profile pic'></img>
+                <div className='User-avatar-container btn-themed-white-black' onClick={showHiddenOptionsFromAvatarContainer}>
+                    <img className='User-avatar-container-icon Icon-xl'
+                         alt='user profile pic' 
+                         src='http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png'/>
+                    <div className='User-avatar-container-info'>
+                        <div className='User-avatar-container-info-name Span-medium-small'>{'André Luiz'}</div>
+                        <FaLock className='Icon-small'/>
+                        <div className='User-avatar-container-info-userName'>{'@andrezzz'}</div>
+                    </div>
+                    <BsThreeDots className='User-avatar-container-dots Icon-small'/>
+                    <div className='User-avatar-container-hidden-options Hidden'>
+                        <span>Adicionar uma conta existente</span>
+                        <span>Sair de {'@andrezzz'}</span>
+                    </div>
+                </div>
 
             </header>
         );
--- web-app/src/components/PrimaryColumn.tsx
+++ web-app/src/components/PrimaryColumn.tsx
@@ -4,48 +4,77 @@ import {FaRegImage,
         FaListUl,
         FaRegSmile,
         FaRegCalendarAlt,
-        FaMapMarkerAlt
+        FaMapMarkerAlt,
+        FaGlobeAmericas,
+        FaChevronDown,
+        FaRegComment,
+        FaHeart,
+        FaShareSquare,
+        FaCircle
 } from 'react-icons/fa';
-import { BsStars } from 'react-icons/bs';
+import { BsStars, BsThreeDots } from 'react-icons/bs';
+import { AiOutlineRetweet } from "react-icons/ai";
 
 function PrimaryColumn() {
 
     const isMediumOrLargeScreen = useMediaQuery({ query: '(min-width: 721px)' });
+
+    function navigateToHome () {
+        window.location.href = "http://localhost:3000/"
+    }
     
     return(
         <div className='Primary-column'>
-
+            
             <div className='Primary-column-header Sticky-top'>
                 {!isMediumOrLargeScreen?
-                    <img className='Primary-column-sticky-header-icon Icon-large' alt='user profile pic' src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"></img>    
+                    <img className='Primary-column-header-icon Icon-large' alt='user profile pic' src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"></img>    
                     :
                     <></>
                 }
-                <span>Página inicial</span>
-                <BsStars className='Primary-column-sticky-header-star Icon-medium'/>
+                <span onClick={navigateToHome}>Página inicial</span>
+                <BsStars className='Primary-column-header-star Icon-large btn-themed-white-black'/>
             </div>
 
             {isMediumOrLargeScreen &&
                 <div className="Make-tweet">
-
+                    
                     <img className='Make-tweet-icon Icon-xl' alt='user profile pic' src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"></img>
 
                     <div className='Make-tweet-container'>
 
                         <div className="Make-tweet-container-field">
-                            <span className="Who-can-view Span-bold-smaller">Qualquer pessoa</span>
+                            <div className="Who-can-view-container btn-themed-white-blue">
+                                <span className="Who-can-view-container-span Span-medium-smaller">Qualquer pessoa</span>
+                                <FaChevronDown className='Who-can-view-container-icon Icon-smaller'/>
+                            </div>
                             <input className='Tweet-content' placeholder='O que está acontecendo?'/>
-                            <span className='Who-can-comment Span-bold-smaller'>Qualquer pessoa pode responder</span>
+                            <div className='Who-can-comment-container btn-themed-white-blue'>
+                                <FaGlobeAmericas className='Who-can-comment-container-icon Icon-small'/>
+                                <span className='Who-can-comment-container-span Span-medium-smaller'>Qualquer pessoa pode responder</span>
+                            </div>
                         </div>
 
                         <div className="Make-tweet-container-toolbar">
                             <div className='Make-tweet-container-toolbar-icons'>
-                                <FaRegImage fill='rgb(29, 155, 240)' className='Icon-small'/>
-                                <FaRegFileImage fill='rgb(29, 155, 240)' className='Icon-small'/>
-                                <FaListUl fill='rgb(29, 155, 240)' className='Icon-small'/>
-                                <FaRegSmile fill='rgb(29, 155, 240)' className='Icon-small'/>
-                                <FaRegCalendarAlt fill='rgb(29, 155, 240)' className='Icon-small'/>
-                                <FaMapMarkerAlt fill='rgb(29, 155, 240)' className='Icon-small'/>
+                                <div className='Make-tweet-container-toolbar-icons-icon btn-themed-white-blue'>
+                                    <FaRegImage fill='rgb(29, 155, 240)' className='Icon-small'/>
+                                </div>
+                                <div className='Make-tweet-container-toolbar-icons-icon btn-themed-white-blue'>
+                                    <FaRegFileImage fill='rgb(29, 155, 240)' className='Icon-small'/>
+                                </div>
+                                <div className='Make-tweet-container-toolbar-icons-icon btn-themed-white-blue'>
+                                    <FaListUl fill='rgb(29, 155, 240)' className='Icon-small'/>
+                                </div>
+                                <div className='Make-tweet-container-toolbar-icons-icon btn-themed-white-blue'>
+                                    <FaRegSmile fill='rgb(29, 155, 240)' className='Icon-small'/>
+                                </div>
+                                <div className='Make-tweet-container-toolbar-icons-icon btn-themed-white-blue'>
+                                    <FaRegCalendarAlt fill='rgb(29, 155, 240)' className='Icon-small'/>
+                                </div>
+                                <div className='Make-tweet-container-toolbar-icons-icon btn-themed-white-blue'>
+                                    <FaMapMarkerAlt fill='rgb(29, 155, 240)' className='Icon-small'/>
+                                </div>
                             </div>
                             <button className='btn-primary btn-themed-blue-white Btn-tweetar'>Tweetar</button>
                         </div>
@@ -59,25 +88,26 @@ function PrimaryColumn() {
 
             </div>
 
-            <article className='Tweet'>
+            <article className='Tweet btn-themed-transparent-black'>
 
                 <div className='Tweet-retweet-or-maybe-you-like'>
-                    <img className="Tweet-retweet-or-maybe-you-like-icon" alt="retweeted icon"/>
-                    <span className="Tweet-retweet-or-maybe-you-like-user">mmag2</span>
+                    {/*<img className="Tweet-retweet-or-maybe-you-like-icon" alt="retweeted icon"/>*/}
+                    <span className="Tweet-retweet-or-maybe-you-like-user"></span>
                 </div>
+
                 <div className='Tweet-wrapper'>
                     <img className='Tweet-wrapper-user-icon Icon-xl' alt='user profile pic' src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"></img>
                     <div className='Tweet-container'>
 
                         <div className="Tweet-container-first-section">
                             <div className="Tweet-container-first-section-infos">
-                                <span className="Tweet-nick">André Luiz</span>
-                                <span className='Tweet-username'>andrezzz</span>
-                                <span>.</span>
+                                <span className="Tweet-nickname Span-bold-small">André Luiz</span>
+                                <span className='Tweet-username'>@andrezzz</span>
+                                <FaCircle className='Tweet-dot'/>
                                 <span className='Tweet-date'>21 de Out</span>
                             </div>
-                            <div className="Tweet-container-first-section-more">
-
+                            <div className="Tweet-container-first-section-more btn-themed-transparent-blue">
+                                <BsThreeDots className='Icon-small'/>
                             </div>
                         </div>
 
@@ -87,51 +117,30 @@ function PrimaryColumn() {
 
                         <img className="Tweet-media" alt="tweet media" src="https://www.enago.com/academy/br/wp-content/uploads/sites/17/2015/06/Twitter-750x330.jpeg"/>
 
-                        <div className="Tweet-container-last-section">
-                            <span className="Tweet-comment">Q</span>
-                            <span className='Tweet-retweet'>Q</span>
-                            <span className='Tweet-like'>Qr</span>
-                            <span className='Tweet-share'>Q</span>
-                        </div>
-
-                    </div>
-                </div>
-
-            </article>
-
-            <article className='Tweet'>
-
-                <div className='Tweet-retweet-or-maybe-you-like'>
-                    <img className="Tweet-retweet-or-maybe-you-like-icon" alt="retweeted icon"/>
-                    <span className="Tweet-retweet-or-maybe-you-like-user">mmag2</span>
-                </div>
-                <div className='Tweet-wrapper'>
-                    <img className='Tweet-wrapper-user-icon Icon-xl' alt='user profile pic' src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png"></img>
-                    <div className='Tweet-container'>
-
-                        <div className="Tweet-container-first-section">
-                            <div className="Tweet-container-first-section-infos">
-                                <span className="Tweet-nick">André Luiz</span>
-                                <span className='Tweet-username'>andrezzz</span>
-                                <span>.</span>
-                                <span className='Tweet-date'>21 de Out</span>
+                        <div className="Tweet-container-toolbar">
+                            <div className='Tweet-container-toolbar-item'>
+                                <div className='Tweet-container-toolbar-item-icon btn-themed-transparent-blue'>
+                                    <FaRegComment className="Icon-small"/>
+                                    </div>
+                                <span className='Tweet-container-toolbar-item-comments'>345</span>
                             </div>
-                            <div className="Tweet-container-first-section-more">
-
+                            <div className='Tweet-container-toolbar-item'>
+                                <div className='Tweet-container-toolbar-item-icon btn-themed-transparent-green'>
+                                    <AiOutlineRetweet className="Icon-small"/>
+                                </div>
+                                <span className='Tweet-container-toolbar-item-retweets'>6.908</span>
                             </div>
-                        </div>
-
-                        <p className='Tweet-text'>
-                            俺哥说要抱着姨干一个晚上的又在哪里？别吹牛批行不行啊？
-                        </p>
-
-                        <img className="Tweet-media" alt="tweet media" src="https://www.enago.com/academy/br/wp-content/uploads/sites/17/2015/06/Twitter-750x330.jpeg"/>
-
-                        <div className="Tweet-container-last-section">
-                            <span className="Tweet-comment">Q</span>
-                            <span className='Tweet-retweet'>Q</span>
-                            <span className='Tweet-like'>Qr</span>
-                            <span className='Tweet-share'>Q</span>
+                            <div className='Tweet-container-toolbar-item'>
+                                <div className='Tweet-container-toolbar-item-icon btn-themed-transparent-pink'>
+                                    <FaHeart className="Icon-small"/>
+                                </div>
+                                <span className='Tweet-container-toolbar-item-likes'>54,4 mil</span>
+                            </div>
+                            <div className='Tweet-container-toolbar-item'>
+                                <div className='Tweet-container-toolbar-item-icon btn-themed-transparent-blue'>
+                                    <FaShareSquare className="Icon-small"/>
+                                </div>
+                            </div>
                         </div>
 
                     </div>
